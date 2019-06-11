242d3bc7f2f3ee06ca28369e59a683112ca56c7d
Sergio Garcia Orozco <srgopezz@gmail.com>
2019-06-11 16:01:37 +0000

    create awesome platformer game

---
+++ projects/platformer/js/init/platform.js
@@ -0,0 +1,48 @@
+(function (window) {
+    'use strict';
+    window.opspark = window.opspark || {};
+    window.opspark.platform = window.opspark.platform || {};
+    
+    let platform = window.opspark.platform;
+    
+    /**
+     * init: This function initializes the platforms for the level.
+     * 
+     * GOAL: Add as many platforms necessary to make your level challenging.
+     * 
+     * Use the platform.create() method to create platforms for the level. 
+     * 
+     * platform.create() takes these arguments:
+     *      
+     *      platform.create(x, y, scaleX, scaleY);
+     * 
+     *      x: The x coordineate for the platform.
+     *      y: The y coordineate for the platform.
+     *      scaleX: OPTIONAL The scale factor on the x-axis, this value will 
+     *              stretch the platform in width.
+     *      scaleY: OPTIONAL The scale factor on the y-axis, this value will 
+     *              stretch the platform in height.
+     */ 
+    platform.init = function (game) {
+        ////////////////////////////////////////////////////////////////////////
+        // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
+        platform.create(250, 300, 1)
+        platform.create(200, 600, 0.3)
+        platform.create(850, 590, 0.3)
+        /*
+         * ground : here, we create a floor. Given the width of of the platform 
+         * asset, giving it a scaleX and scaleY of 2 will stretch it across the 
+         * bottom of the game.
+         */
+        platform.create(0, game.world.height - 32, 3, 2);
+
+        // example:
+        platform.create(525, 500, 0.4);
+        platform.create(850, 400, 0.3);
+        
+        
+        
+        // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////
+    };
+})(window);
